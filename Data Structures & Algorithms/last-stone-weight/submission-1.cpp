e095b97bdde5144359c1f1879a31297ad5f24133
Aditya Abhiram Uppuluri <abhi.uppuluri@gmail.com>
2026-06-22 09:53:49 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(int i=0; i<stones.size(); i++) pq.push(stones[i]);
+        while(pq.size()>1) {
+            // if(pq.size()>=2){
+                int first = pq.top();
+                pq.pop();
+                int second = pq.top();
+                pq.pop();
+
+                if(first==second) {
+                    continue;
+                }
+                else {
+                    pq.push(abs(first-second));
+                }
+            // }
+        }
+        if(pq.empty()) return 0;
+        else return pq.top();
+    }
+
+    
+};
